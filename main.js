25a8b6ca6458b9203a8b8c79d067f0c5ed5c29b4
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-02-03 12:01:40 -0600

    lists persist in localstorage, can be transferred via url

--- main.js
+++ main.js
@@ -1,3 +1,79 @@
+(function() {
+
+	var storageAvailable = function (type) {
+		try {
+			var storage = window[type],
+				x = '__storage_test__';
+			storage.setItem(x, x);
+			storage.removeItem(x);
+			return true;
+		}
+		catch(e) {
+			return false;
+		}
+	}
+
+	if ( storageAvailable('localStorage') ) {
+		window.useStorage = true;
+	}
+	else {
+		window.useStorage = false;
+	}
+
+});
+
+// function to get query string parameters
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
+window.useStorage = true;
+window.localStorageColumnKey = "githubIssueIdColumn";
+function getColumnFromId ( id ) {
+	if ( window.useStorage ) {
+		var column = localStorage.getItem( localStorageColumnKey + id );
+		returnVal = column ? column : 0;
+	}
+	else {
+		returnVal = 0;
+	}
+	return returnVal;
+}
+
+function setColumnFromId ( id, columnNum ) {
+	if ( window.useStorage ) {
+		localStorage.setItem( localStorageColumnKey + id, columnNum );
+	}
+}
+
+function getColumnsFromQueryString () {
+	var qs = $.QueryString['columns'];
+	var output = {};
+	if ( ! qs ) {
+		return output;
+	}
+	var idColString = qs.split(";");
+	for (var j in idColString) {
+		if ( ! idColString[j].trim() ) {
+			continue; // if empty string, do nothing
+		}
+		pair = idColString[j].split(':');
+		id = pair[0];
+		col = pair[1];
+		output[id] = col;
+	}
+	return output;
+}
 
 function getIssues () {
 	var milestoneNum = $("#milestone").val();
@@ -7,27 +83,51 @@ function getIssues () {
 		{},
 		function( issues ) {
 			resetColumns();
-
+			var columnNum;
+			var highestColumn = $(".sortable-list").size() - 1;
 			for( var i in issues ) {
 				var issue = issues[i];
-
-				$("#sortable0").append(
-					"<li class='ui-state-default'><a href='" + issue.html_url + "'>#" + issue.number + "</a> " + issue.title + "</li>");
+				columnIndex = getColumnFromId( issue.id );
+				if ( columnIndex > highestColumn ) {
+					addColumn( columnIndex ); // will add columns up to this column index
+				}
+				$("#sortable" + columnIndex).append(
+					"<li class='ui-state-default' github-id='" + issue.id + "'><a href='" + issue.html_url + "'>#" + issue.number + "</a> " + issue.title + "</li>");
 			}
 		}
 	);
 }
 
-function addColumn () {
-	var index = $(".sortable-list").size();
+function addColumn ( fillToIndex ) {
+
+	var newIndex = $(".sortable-list").size();
 
 	$("#add-new").before(
-		"<div class='list-container'><div class='title-wrapper'><input type='text' class='title-box' /></div><ul id='sortable" + index + "' class='droptrue sortable-list'></ul></div>"
+		"<div class='list-container'><div class='title-wrapper'><input type='text' class='title-box' /></div><ul id='sortable" + newIndex + "' column-num='" + newIndex + "' class='droptrue sortable-list'></ul></div>"
 	);
 
-	$( "#sortable" + index ).disableSelection().sortable({
-		connectWith: "ul"
+	$( "#sortable" + newIndex ).disableSelection().sortable({
+		connectWith: "ul",
+		stop: function( event, ui ) { // FIXME: this fires twice.
+			setColumnFromId( ui.item.attr( 'github-id' ), ui.item.parent().attr( 'column-num' ) );
+			updateURI();
+		}
 	});
+
+	if ( fillToIndex && newIndex < fillToIndex ) {
+		addColumn( fillToIndex );
+	}
+}
+
+function updateURI () {
+	var uriString = '';
+	$(".sortable-list li").each( function( i, e ) {
+		var $e = $(e);
+		var id = $e.attr( 'github-id' );
+		var col = $e.parent().attr( 'column-num' );
+		uriString += id + ":" + col + ";";
+	});
+	window.history.pushState("some object", "some title", location.pathname + "?columns=" + uriString);
 }
 
 function resetColumns() {
@@ -53,6 +153,19 @@ function resetColumns() {
 
 }
 
+// Taken from http://stackoverflow.com/questions/5999118/add-or-update-query-string-parameter
+// Thanks!
+// function updateQueryStringParameter(uri, key, value) {
+// 	var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+// 	var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+// 	if (uri.match(re)) {
+// 		return uri.replace(re, '$1' + key + "=" + value + '$2');
+// 	}
+// 	else {
+// 		return uri + separator + key + "=" + value;
+// 	}
+// }
+
 // setup
 $(function(){
 	$("#add-column").button({
@@ -61,6 +174,13 @@ $(function(){
 	}).click( addColumn );
 
 	$("#get-issues").button().click( getIssues );
+
+	// preset IDs to certain columns per query string
+	var presets = getColumnsFromQueryString();
+	for ( var id in presets ) {
+		setColumnFromId( id, presets[id] );
+	}
+
 
 	$.getJSON(
 		"https://api.github.com/repos/enterprisemediawiki/meza/milestones",
